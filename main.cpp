9b5351fd363c85dca9b5f13967d8cb553d116b9c
hafidhhusna <114789732+hafidhhusna@users.noreply.github.com>
2022-10-01 10:09:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+// TUGAS PEMROGRAMAN TETI LABSKILL 2022 //
+// HAFIDH HUSNA_MAXWELL //
+
 #include <iostream>
 
 using namespace std;
@@ -5,8 +8,11 @@ using namespace std;
 int
 main ()
 {
-  float temp;
-  int batuk, sesak_napas, anosmia; 
+//Variables Declaration
+  float temp; //Since The Input Of The Data Are In Decimal, Variable Declaration in Float Data Type
+  int batuk, sesak_napas, anosmia; //Since The Input Of The Data Are In Integers, Variable Declaration in Integer Data Type
+//Program Starts
+//User's Input
   cout << "Silahkan Masukkan Suhu Tubuh Anda : \n";
   cin >> temp;
   cout <<
@@ -18,18 +24,19 @@ main ()
   cout << "1. Ya\n";
   cout << "2. Tidak\n";
   cin >> sesak_napas;
-  if (temp >= 37.5)
-    if (anosmia == 1)
-      if (sesak_napas == 1)
-	cout <<
+//Checking The Inputs From User
+  if (temp >= 37.5) //First Condition
+    if (anosmia == 1) //Second Condition
+      if (sesak_napas == 1) //Third Condition
+	cout << //If All Three Conditions Are Met
 	  "Kesimpulan : Anda Memiliki Gejala COVID-19, Segera Lakukan Tes PCR dan Lakukan Konsultasi dengan Dokter\n";
-      else
+      else //If Only The First and Second Conditions Are Met
 	cout <<
 	  "Kesimpulan : Anda Memiliki Gejala Ringan COVID-19, Segera Lakukan Tes PCR atau Isolasi Mandiri\n";
-    else
+    else //If Only The First Condition Are Met
       cout <<
 	"Kesimpulan : Suhu Tubuh Anda Tinggi, Pulihkan Diri dengan Istirahat di Rumah\n";
-  else
+  else //If There's No Condition Are Met
     cout << "Kesimpulan : Aman";
   return 0;
 }
